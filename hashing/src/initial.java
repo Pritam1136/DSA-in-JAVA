83efc1a113aded4e2e0f3cc4d990dd2321b45084
Pritam Roy <pritamroy1136@gmail.com>
2023-11-20 21:03:47 +0530

    try with added resouces

--- hashing/src/initial.java
+++ hashing/src/initial.java
@@ -11,15 +11,16 @@ public class initial {
         return count;
     }
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the number to search:");
-        int n = in.nextInt();
-        int [] arr = new int[10];
-        System.out.println("Enter 10 numbers.");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = in.nextInt();
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Enter the number to search:");
+            int n = in.nextInt();
+            int [] arr = new int[10];
+            System.out.println("Enter 10 numbers.");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = in.nextInt();
+            }
+            int count = func(n,arr);
+            System.out.println("The numbers of times it appear =" + count);
         }
-        int count = func(n,arr);
-        System.out.println("The numbers of times it appear =" + count);
     }
 }
