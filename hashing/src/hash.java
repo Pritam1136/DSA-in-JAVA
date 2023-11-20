59d349dfc8ce2cdd9a543bd25a91a764e3bcc1a6
Pritam Roy <pritamroy1136@gmail.com>
2023-11-20 10:29:41 +0530

    Started hashing

---
+++ hashing/src/hash.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class hash {
+    static Scanner in = new Scanner(System.in);
+    static void hash(int[] arr, int n){
+        //        precompute
+        int hash[] = new int[100];
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]] += 1;
+        }
+        int query = 0;
+        System.out.println("Enter the numbers of query(ies) :");
+        query = in.nextInt();
+        while (query > 0){
+            int number;
+            System.out.println("Enter the numbers :");
+            number =in.nextInt();
+//            fetch method
+            System.out.println(hash[number]);
+            query--;
+        }
+
+    }
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter the length of array :");
+        n = in.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter the elements in array :");
+        for (int i = 0; i <n; i++) {
+            arr[i] = in.nextInt();
+        }
+        hash(arr,n);
+    }
+}
