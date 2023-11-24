eb0075d0efecda727fe24d4a8086db0e7181f6f9
Pritam Roy <pritamroy1136@gmail.com>
2023-11-24 20:23:02 +0530

    started array

--- sorting/src/quickSort.java
+++ sorting/src/quickSort.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class quickSort {
-    static void quick(){
+    static void quick(int[] arr){
 
     }
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the length of array.");
+        int n = in.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter the element of array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        quick(arr);
     }
 }
