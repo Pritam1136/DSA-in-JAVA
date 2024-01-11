02ac5fe05b8e799a38d5d4b44ae0d6380e40e4b3
Pritam Roy <pritamroy1136@gmail.com>
2024-01-11 20:03:37 +0530

    Renamed maxConsecutive

--- array/src/maxConsecutive.java
+++ array/src/maxConsecutive.java
@@ -1,5 +1,5 @@
-public class maxConcicutive {
-    static void consicutive(int[] arr){
+public class maxConsecutive {
+    static void consecutive(int[] arr){
         int flag = 0;
         int max = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -14,6 +14,6 @@ public class maxConcicutive {
     }
     public static void main(String[] args) {
         int [] arr = {1,1,1,1,1,1,1,1,0,0,0,1,0,0,1,1,1,0,0,0,0,1,1,1,1,1,1,2,1,1,1,1};
-        consicutive(arr);
+        consecutive(arr);
     }
 }
