6d5fa839e3ee240e04364ec13746a391ef273a06
Pritam Roy <pritamroy1136@gmail.com>
2024-01-12 10:22:42 +0530

    sum k

---
+++ array/src/sumKPositive.java
@@ -0,0 +1,27 @@
+public class sumKPositive {
+    static int sumL(int[] arr, int k){
+        int left = 0;
+        int right = 0;
+        long sum = arr[0];
+        int maxLen = 0;
+        int n = arr.length;
+        while (right < n){
+            while (left < right && sum > k){
+                sum -= arr[left];
+                left++;
+            }
+            if (sum == k){
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right ++;
+            if (right < n)
+                sum += arr[right];
+        }
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,1,2,1};
+       int answer = sumL(arr,3);
+        System.out.println(answer);
+    }
+}
