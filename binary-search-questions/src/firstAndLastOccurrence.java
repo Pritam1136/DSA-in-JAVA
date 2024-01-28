f60e123752c9d7b97b5e96d21c159d07d176bd38
Pritam Roy <pritamroy1136@gmail.com>
2024-01-28 18:55:40 +0530

    removed unused variable

--- binary-search-questions/src/firstAndLastOccurrence.java
+++ binary-search-questions/src/firstAndLastOccurrence.java
@@ -1,38 +1,36 @@
 public class firstAndLastOccurrence {
     static int fistOccurrence(int[] arr, int target){
         int n = arr.length-1;
-        int low = 0,high = n, ans = n;
+        int low = 0,high = n;
         while (low <= high){
             int mid = (low + high) / 2;
             if (arr[mid] >= target){
-                ans = mid;
                 high = mid - 1;
             }
             else {
                 low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
     static int lastOccurrence(int[] arr, int target){
         int n  = arr.length - 1;
-        int low = 0,high = n, ans = n;
+        int low = 0,high = n;
         while (low <= high){
             int mid = (low + high) / 2;
             if (arr[mid] > target){
-                ans = mid;
                 high = mid - 1;
             }
             else {
                 low = mid + 1;
             }
         }
-       return ans;
+       return low;
     }
 
     public static void main(String[] args) {
         int[] arr = {2,4,6,8,8,8,8,11,13};
-        int target = 14;
+        int target = 8;
        int a = fistOccurrence(arr, target);
         int b = lastOccurrence(arr, target);
         b--;
