f4b8f7e770f2c3cb47ccd366014bd4db98a87339
Pritam Roy <pritamroy1136@gmail.com>
2024-01-29 21:21:06 +0530

    Made a small modification

--- binary-search-questions/src/KthMissing.java
+++ binary-search-questions/src/KthMissing.java
@@ -5,7 +5,7 @@ public class KthMissing {
         int low = 0, high = arr.length - 1;
         while (low <= high){
             int mid = (low + high) / 2;
-            int missing = arr[mid] - mid + 1;
+            int missing = arr[mid] - (mid + 1);
             if (missing < k){
                 low = mid + 1;
             } else {
