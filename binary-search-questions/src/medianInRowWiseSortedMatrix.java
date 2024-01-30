632b07993f7f8940e4f9bdb546b44610bc85bb01
Pritam Roy <pritamroy1136@gmail.com>
2024-01-30 17:41:55 +0530

    on hold

---
+++ binary-search-questions/src/medianInRowWiseSortedMatrix.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class medianInRowWiseSortedMatrix {
+//    static int search(int[][] arr){
+//        int n = arr.length;
+//        int m = arr[0].length;
+//        int low =
+//    }
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter the target");
+//        int target = in.nextInt();
+//        System.out.println("Enter the number of row");
+//        int n  = in.nextInt();
+//        System.out.println("Enter the number of column");
+//        int m = in.nextInt();
+//        int[][] arr = new int[n][m];
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                arr[i][j] = in.nextInt();
+//            }
+//        }
+//        int ans = search(arr);
+//        System.out.println(ans);
+//    }
+//}
