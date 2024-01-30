91de41064477762275893e7bb69d668e7f685900
Pritam Roy <pritamroy1136@gmail.com>
2024-01-30 18:04:00 +0530

    Added some theories

--- trees/src/Main.java
+++ trees/src/Main.java
@@ -3,3 +3,8 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+//Full binary tree - Either has 2 or 0 children.
+//Complete binary tree - All levels are filled except the last level.Last level has all nodes as left as possible.
+//Perfect binary tree - All leaf node in same level.
+//Balance binary tree - Height of the tree at max is log(n).
+//Degenerate binary tree - Every node has only one child.
