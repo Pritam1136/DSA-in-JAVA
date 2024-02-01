4ce58fb6c10fcd1629ad2855729dbfc095338261
Pritam Roy <pritamroy1136@gmail.com>
2024-02-01 18:34:37 +0530

    Count the number of steps

---
+++ recursion/src/steps.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class steps {
+    static int count(int a, int helper){
+        if (a == 0){
+            return helper;
+        }
+        helper ++;
+        if (a % 2 == 0){
+            a = a / 2;
+            return count(a, helper);
+        } else {
+            return count(a - 1 , helper);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int a = in.nextInt();
+        int b = count(a, 0);
+        System.out.println("Number of steps are : " + b);
+    }
+}
