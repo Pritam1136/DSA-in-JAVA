fa6a9cbf9172ddddbc94cf1e625dcc42c7540ef3
Pritam Roy <pritamroy1136@gmail.com>
2024-02-02 22:52:30 +0530

    Linear search using recursion

---
+++ recursion/src/linearSearch.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class linearSearch {
+    static boolean Search(int[] arr, int n, int target){
+        if (n >= arr.length)
+            return false;
+        if (arr[n] == target)
+            return true;
+        else
+            return Search(arr, ++n, target);
+    }
+    static ArrayList SearchD (int [] arr, int n , int target, ArrayList<Integer> list){
+        if (n >= arr.length)
+            return list;
+        else if (arr[n] == target)
+            list.add(n);
+        return SearchD(arr, ++n, target, list);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the length of array :");
+        int n = in.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter array elements :");
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println("Enter the target");
+        int target = in.nextInt();
+        System.out.println(Search(arr,0, target));
+        System.out.println(SearchD(arr, 0 , target, new ArrayList<>()));
+    }
+}
