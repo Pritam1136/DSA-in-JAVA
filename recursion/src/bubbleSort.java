3f0498ae251bcaf37ef371e935f1e4822ff15e9c
Pritam Roy <pritamroy1136@gmail.com>
2024-02-03 18:51:13 +0530

    Bubble sort using recursion

---
+++ recursion/src/bubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bubbleSort {
+        static void print( int[] arr, int count, int row){
+            if (count == arr.length)
+                return;
+            for (int j = 0; j < row; j++) {
+                for (int i = 0; i < count; i++) {
+                    if (arr[i] > arr[i+1]){
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+            print(arr, ++count, row);
+        }
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter the length of array");
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter array elements");
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+            }
+            print(arr ,0, n);
+        }
+}
