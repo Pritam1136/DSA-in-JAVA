410ebb3901f6dc86ece72eb226f585186f104c4b
Pritam Roy <pritamroy1136@gmail.com>
2024-02-03 18:54:40 +0530

    Bubble sort using recursion

--- recursion/src/bubbleSort.java
+++ recursion/src/bubbleSort.java
@@ -8,9 +8,9 @@ public class bubbleSort {
             for (int j = 0; j < row; j++) {
                 for (int i = 0; i < count; i++) {
                     if (arr[i] > arr[i+1]){
-                        int temp = arr[i];
-                        arr[i] = arr[i + 1];
-                        arr[i + 1] = temp;
+                       arr[i] = arr[i] + arr[i + 1];
+                       arr[i + 1] = arr[i] - arr[i + 1];
+                       arr[i] = arr[i] - arr[i + 1];
                     }
                 }
             }
