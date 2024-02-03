172e52273baabe913c33b6155230255ed4176d80
Pritam Roy <pritamroy1136@gmail.com>
2024-02-03 19:38:20 +0530

    made some corrections

--- recursion/src/bubbleSort.java
+++ recursion/src/bubbleSort.java
@@ -2,22 +2,27 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class bubbleSort {
-        static void sort( int[] arr, int count, int row){
-            if (count == arr.length)
-                return;
-            for (int j = 0; j < row; j++) {
-                for (int i = 0; i < count; i++) {
-                    if (arr[i] > arr[i+1]){
-                       arr[i] = arr[i] + arr[i + 1];
-                       arr[i + 1] = arr[i] - arr[i + 1];
-                       arr[i] = arr[i] - arr[i + 1];
-                    }
+    static void sort(int[] arr, int row) {
+        boolean swapped;
+        if (row == 0)
+            return;
+        for (int i = row; i > 0; i--) {
+            swapped = false;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                    swapped = true;
                 }
             }
             System.out.println(Arrays.toString(arr));
-            sort(arr, ++count, row);
+            if (!swapped)
+                break;
+            sort(arr, --row);
         }
-        public static void main(String[] args) {
+    }
+        public static void main (String[]args){
             Scanner in = new Scanner(System.in);
             System.out.println("Enter the length of array");
             int n = in.nextInt();
@@ -26,6 +31,6 @@ public class bubbleSort {
             for (int i = 0; i < n; i++) {
                 arr[i] = in.nextInt();
             }
-            sort(arr ,0, n);
+            sort(arr,arr.length - 1);
         }
-}
+    }
