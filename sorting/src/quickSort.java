b122206b5b66e50373f5bea473356f8a68d27a99
Pritam Roy <pritamroy1136@gmail.com>
2024-02-04 19:26:11 +0530

    Completed quick sort

--- sorting/src/quickSort.java
+++ sorting/src/quickSort.java
@@ -1,8 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class quickSort {
-    static void quick(int[] arr){
+    static void quick(int[] arr, int low, int high){
+        if (low >= high)
+            return;
 
+        int start = low;
+        int end = high;
+        int mid =  start + (end - start) / 2;
+        int pivot = arr[mid];
+
+        while (start <= end){
+            while (arr[start] < pivot)
+                start ++;
+            while (arr[end] > pivot)
+                end --;
+            if (start <= end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        quick(arr, low, end);
+        quick(arr, start, high);
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -13,6 +36,11 @@ public class quickSort {
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        quick(arr);
+        quick(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 }
+/*
+ Time complexity = O(n^2)
+ Space complexity = O(1)
+*/
