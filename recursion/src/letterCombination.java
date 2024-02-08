2dd4d2a4e2dd8d319b31eb6d53a8c46dbaca1ad2
Pritam Roy <pritamroy1136@gmail.com>
2024-02-08 21:27:05 +0530

    return type

--- recursion/src/letterCombination.java
+++ recursion/src/letterCombination.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class letterCombination {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         pad("", "12");
         System.out.println(pad1("", "123"));
     }
