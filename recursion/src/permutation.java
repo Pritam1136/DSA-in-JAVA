3a845822761b7a12261353cc716864acc44671b7
Pritam Roy <pritamroy1136@gmail.com>
2024-02-08 21:16:09 +0530

    return type

--- recursion/src/permutation.java
+++ recursion/src/permutation.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class permutation {
     static void permu(String q, String p) {
@@ -14,7 +14,23 @@ public class permutation {
             permu(f+ch+s,p.substring(1));
         }
     }
+    static ArrayList<String> permu1 (String p, String up){
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0,i);
+            String s = p.substring(i);
+            list.addAll(permu1(f+ch+s, up.substring(1)));
+        }
+        return list;
+    }
     public static void main(String[] args) {
+        System.out.println(permu1("", "abc"));
         permu("", "abc");
     }
 }
