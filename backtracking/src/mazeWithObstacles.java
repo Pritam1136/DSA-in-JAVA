e839727df93c29f2d042e23e217afeea870277ea
Pritam Roy <pritamroy1136@gmail.com>
2024-02-09 15:49:29 +0530

    Maze problem with obstacle

---
+++ backtracking/src/mazeWithObstacles.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class mazeWithObstacles {
+    public static void main(String[] args) {
+        boolean[][] board = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true}
+        };
+mazeWithObstacle("", board, 0, 0);
+    }
+    static void mazeWithObstacle(String p, boolean[][] maze, int row, int col){
+        if (row == maze.length - 1 && col == maze[0].length - 1){
+            System.out.println(p);
+            return ;
+        }
+        if (!maze[row][col]){
+            return ;
+        }
+        if (row < maze.length - 1){
+            mazeWithObstacle(p+'V',maze, row+1, col);
+        }
+        if (col < maze[0].length - 1){
+            mazeWithObstacle(p+'H',maze, row, col+1);
+        }
+    }
+}
