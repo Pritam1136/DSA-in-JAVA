7e8fac95278d5a76c529eca00a6c7a58c50b873f
Pritam Roy <pritamroy1136@gmail.com>
2024-09-11 01:21:55 +0530

    Done 5 more questions

--- arrays/src/IntersectionOfTwoSortedArray.java
+++ arrays/src/IntersectionOfTwoSortedArray.java
@@ -10,14 +10,12 @@ public class IntersectionOfTwoSortedArray {
         int i = 0, j = 0;
 
         System.out.print("Intersection of the two arrays: ");
-        // Use two-pointer technique to find intersection
         while (i < arr1.length && j < arr2.length) {
             if (arr1[i] < arr2[j]) {
-                i++; // Move pointer i if arr1[i] is smaller
+                i++;
             } else if (arr2[j] < arr1[i]) {
-                j++; // Move pointer j if arr2[j] is smaller
+                j++;
             } else {
-                // If both elements are equal, it's part of the intersection
                 System.out.print(arr1[i] + " ");
                 i++;
                 j++;
