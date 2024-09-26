b2686234a5b9ebc629083de92294111980359f79
Pritam Roy <pritamroy1136@gmail.com>
2024-09-26 17:39:06 +0530

    Binary search questions

--- binary-search/src/LowerBoundAndUpperBound.java
+++ binary-search/src/LowerBoundAndUpperBound.java
@@ -1,35 +1,31 @@
 public class LowerBoundAndUpperBound {
     static int lowerBound(int[] arr, int length, int target){
         int low = 0, high = length-1;
-        int ans = length;
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
             if (target <= arr[mid]) {
-                ans = mid;
                 high = mid - 1;
             }
             else {
                 low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
     static int upperBound(int[] arr, int length, int target){
         int low = 0, high = length-1;
-        int ans = length;
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
             if (target < arr[mid]) {
-                ans = mid;
                 high = mid - 1;
             }
             else {
                 low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
     public static void main(String[] args) {
         int [] arr = {3,4,6,7,9,10,12,16,17};
