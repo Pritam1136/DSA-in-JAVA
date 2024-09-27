d704e3db1c9818a23df5a43a59b8706b4a95533a
Pritam Roy <pritamroy1136@gmail.com>
2024-09-27 18:07:53 +0530

    Kth missing number

--- binary-search/src/KthMissingNum.java
+++ binary-search/src/KthMissingNum.java
@@ -1,7 +1,17 @@
 public class KthMissingNum {
     public static void main(String[] args) {
-        int[] arr = {2,3,4,7,11};
-        int k = 5;
-
+        int[] arr = {4,7,11};
+        int k = 3, missing = 0;
+        int low = 0, high = arr.length - 1;
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            missing = arr[mid] - (mid + 1);
+            if (missing < k){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        System.out.println(low + k);
     }
 }
