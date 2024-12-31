7bf65549ec4c07b4cf4cf7fd97824670bc820c60
Pritam Roy <pritamroy1136@gmail.com>
2024-12-31 11:43:47 +0530

    Plymorphysm

---
+++ oop/src/Polymorphism/Square.java
@@ -0,0 +1,7 @@
+package Polymorphism;
+
+public class Square {
+    void area(){
+        System.out.println("Area is square of side.");
+    }
+}
---
+++ oop/src/Polymorphism/Triangle.java
@@ -0,0 +1,7 @@
+package Polymorphism;
+
+public class Triangle {
+    void area(){
+        System.out.println("Area is 1/2*l*b");
+    }
+}
